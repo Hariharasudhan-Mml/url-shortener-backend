5fb1ce9fac81771cc431a4f84afdd4bf1480fdf5
Hariharasudhan-Mml <havocshari@gmail.com>
2022-04-23 20:10:48 +0530

    updated

--- Routes/Redirect.js
+++ Routes/Redirect.js
@@ -1,30 +1,30 @@
 const router = require('express').Router();
 const Data = require('../Models/Datamodel');
-const open=require('open')
+const open = require('open')
 
-router.get('/:shorturl',async (req, res) => {
-try { 
-     const shorturl = req.params.shorturl;
-    console.log(shorturl)
-    const data = await Data.findOne({ shortURL: shorturl });
-    console.log("data=="+data)
-    if (!data) {
-        return res.json({ msg: "This is not a valid URL" })
+router.get('/:shorturl', async (req, res) => {
+    try {
+        const shorturl = req.params.shorturl;
+        console.log(shorturl)
+        const data = await Data.findOne({ shortURL: shorturl });
+        console.log("data==" + data)
+        if (!data) {
+            return res.json({ msg: "This is not a valid URL" })
+        }
+        else {
+            data.clicks++;
+           await data.save()
+            console.log(data.clicks)
+            await open(data.longURL)
+            res.status(200)
+        }
+    } catch (error) {
+        res.json({ msg: error.message })
     }
-    else {
-        data.clicks++;
-        data.save()
-        console.log(data.clicks)
-       await  open(data.longURL)
-  
-    }
-} catch (error) {
-    res.json({msg:error.message})
-}
-  
+
 
 
 });
 
 
-module.exports=router
+module.exports = router
